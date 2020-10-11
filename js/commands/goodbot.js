197d67111963018f28d15079fc5d3548a1113b47
Rian Morgan <rianmorgan@rians-mbp.home>
2020-10-11 23:37:46 +0100

    removed secrets

--- js/commands/goodbot.js
+++ js/commands/goodbot.js
@@ -2,6 +2,6 @@ module.exports = {
     name:'goodbot',
     description:'goodbot',
     execute (message, args) {
-        message.channel.send("Thx cunt")
+        message.channel.send("Thx")
     },
 }
